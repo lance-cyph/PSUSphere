0d26580168f5981c12d3491845f19ea81f902079
lance-cyph <slance.astig@gmail.com>
2026-09-02 10:02:57 +0800

    Change

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%%126zqx+y-8$nn%ra((bcx!-$r&*e(mbje2^2&jhg)j)-58n_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['lanica.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
