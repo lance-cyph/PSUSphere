f450bf37be7a956140a3a79d2261d253e48548bf
lance-cyph <slance.astig@gmail.com>
2026-09-02 09:18:46 +0800

    Finished 05 Query Set, Context Data, Ordering

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,19 +1,49 @@
+from django.core.serializers import python
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from studentorg.models import Organization, OrgMember, Student, College, Program
+from django.db.models import Q
+from django.utils import timezone
 
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
 
+def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context["total_students"] = Student.objects.count()
+    
+    today = timezone.now().date()
+    count = (
+        OrgMember.objects.filter(date_joined__year=today.year)
+        .values("student")
+        .distinct()
+        .count()
+    )
+    
+    context["students_joined_this_year"] = count
+    return context
+
 class OrganizationList(ListView):
     model = Organization
     context_object_name = 'organizations'
     template_name = 'org_list.html'
     paginate_by = 5
+    ordering = ["college__college_name","name"]
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        query = self.request.GET.get('q')
+
+        if query:
+            qs = qs.filter(
+                Q(name__icontains=query) |
+                Q(description__icontains=query)
+            )
+        return qs
 
 class OrganizationUpdateView(UpdateView):
     model = Organization
@@ -106,6 +136,13 @@ class ProgramList(ListView):
     context_object_name = 'programs'
     template_name = 'program_list.html'
     paginate_by = 5
+
+    def get_ordering(self):
+        allowed = ["prog_name", "college__college_name"]
+        sort_by = self.request.GET.get("sort_by")
+        if sort_by in allowed:
+            return sort_by
+        return "prog_name"
 
 class ProgramCreateView(CreateView):
     model = Program
