1fa97e96b6efea826630d9ffcbc87966c0a9b990
lance-cyph <slance.astig@gmail.com>
2026-09-06 23:28:44 +0800

    Update

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -32,7 +32,7 @@ else:
     DEBUG = True
     SITE_ID = 2  # Local site ID (127.0.0.1:8000)
 
-ALLOWED_HOSTS = ['lanica.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'lanica.pythonanywhere.com']
 
 
 # Application definition
