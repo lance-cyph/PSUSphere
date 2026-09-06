1cab4d62dafe4fffec2a4e0aa4970f4ccf81b9b2
lance-cyph <slance.astig@gmail.com>
2026-09-06 13:31:57 +0800

    Updated setting.py

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-%%126zqx+y-8$nn%ra((bcx!-$r&*e(mbje2^2&jhg)j)-58n_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'lanica.pythonanywhere.com']
+ALLOWED_HOSTS = ['lanica.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
